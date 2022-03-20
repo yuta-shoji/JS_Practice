30b6d5d5bc5db71d95520f118cd74e1709b84876
yuta-shoji <90593209+yuta-shoji@users.noreply.github.com>
2022-03-20 16:56:51 +0900

    Update getNumberName.js

--- getNumberName.js
+++ getNumberName.js
@@ -1,14 +1,14 @@
 function getNumberName(num){
-    const ty = ['ten' ,'twenty' ,'thirty' ,'forty' ,'fifty' ,'sixty' ,'seventy' ,'eighty' ,'ninety'];
-    const teen = ['eleven' ,'twelve' ,'thirteen' ,'fourteen' ,'fifteen' ,'sixteen' ,'seventeen' ,'eighteen' ,'nineteen'];
-    const underTen ['zero' ,'one' ,'two' ,'three' ,'four' ,'five' ,'six' ,'seven' ,'eight' ,'nine'];
+    const ty = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const teen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const underTen = ['zero', 'one', 'two', 'three','four', 'five', 'six', 'seven', 'eight', 'nine'];
     const thousand = String(num).charAt(String(num).length - 4);
     const hundred = String(num).charAt(String(num).length - 3);
     const tenth = String(num).charAt(String(num).length - 2);
     const first = String(num).charAt(String(num).length - 1);
     let result = "";
     if (String(num).length >= 4){
-        result += `${underTen[thousand]} thousand`;
+        result += `${underTen[thousand]} thousand `;
         if (String(num) % 1000 === 0){
             return result.trim();
         }
@@ -27,7 +27,7 @@ function getNumberName(num){
             result += `${teen[first - 1]}`;
             return result;
         } else if (tenth + first > 20){
-            result += `${ty[tenth - 1]}`;
+            result += `${ty[tenth - 1]} `;
         }
     }
     return result += `${underTen[first]}`;
