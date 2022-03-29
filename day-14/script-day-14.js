a201bc12e90de6f457ba6948868ad13e030276af
yuta-shoji <90593209+yuta-shoji@users.noreply.github.com>
2022-03-29 21:53:04 +0900

    Update script-day-14.js

--- day-14/script-day-14.js
+++ day-14/script-day-14.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 // 1行目に記載している 'use strict' は削除しないでください
 
-// function test(actual, expected) {
-//     if (JSON.stringify(actual) === JSON.stringify(expected)) {
-//         console.log("Yay! Test PASSED.");
-//     } else {
-//         console.error("Test FAILED. Keep trying!");
-//         console.log("    actual: ", actual);
-//         console.log("  expected: ", expected);
-//         console.trace();
-//     }
-// }
+function test(actual, expected) {
+    if (JSON.stringify(actual) === JSON.stringify(expected)) {
+        console.log("Yay! Test PASSED.");
+    } else {
+        console.error("Test FAILED. Keep trying!");
+        console.log("    actual: ", actual);
+        console.log("  expected: ", expected);
+        console.trace();
+    }
+}
 
 //warmUp
 //引数に x を取り、関数を返す divide という関数を作成してください。
@@ -23,7 +23,7 @@
 function divide(x) {
     return function (y) {
         return x / y;
-    }
+    };
 }
 
 const divide2 = divide(2);
@@ -32,19 +32,18 @@ const divide2 = divide(2);
 // test(divide(2)(1), 2);
 // test(divide(6)(2), 3);
 
-
 //#########################################################################################################################
 //基礎演習
 
 const myMathObject = {
     sum: function (...args) {
         let result = 0;
-        args.map(num => result += num);
+        args.map((num) => (result += num));
         return result;
     },
     product: function (...args) {
         let result = 1;
-        args.map(num => result *= num);
+        args.map((num) => (result *= num));
         return result;
     },
     isEven: function (num) {
@@ -53,18 +52,73 @@ const myMathObject = {
     getNumbers: function (...args) {
         const result = [];
         for (const arr of args) {
-            if (typeof arr === "number") {
-                result.push(arr);
-            }
+        if (typeof arr === "number") {
+            result.push(arr);
+        }
         }
         return result;
     },
     abs: function (num) {
         if (num < 0) {
-            return num * - 1;
+        return num * -1;
         } else {
-            return num;
+        return num;
         }
+    },
+    power: function (base, exponent) {
+        let result = 1;
+        if (exponent < 0) {
+            if (base === 0) {
+                return Infinity;
+            } else {
+                for (let i = 1; i <= exponent * -1; i++) {
+                    result /= base
+                }
+            }
+        } else if (exponent > 0) {
+            for (let i = 1; i <= exponent; i++) {
+                result *= base
+            }
+        } else {
+            return 1;
+        }
+        return result;
+    },
+    ceil: function(num) {
+        return (num | 0) + 1
+    },
+    floor: function(num) {
+        return num | 0
+    },
+    round: function (num) {
+        if (num - (num | 0) < 0.5) {
+            return myMathObject.floor(num);
+        } else {
+            return myMathObject.ceil(num);
+        }
+    },
+    primeFactorization: function (num) {
+        let result = {};
+        if (num === 1) {
+            return {"1": 1};
+        }
+        let init = 2;
+        while (num !== 1) {
+            let i = init;
+            while (i < Number.MAX_SAFE_INTEGER) {
+                if (num % i === 0) {
+                    num /= i;
+                    if (!(result[i] > 0)) {
+                        result[i] = 0;
+                    }
+                    result[i]++;
+                    break;
+                }
+                i++;
+            }
+            init = i;
+        }
+        return result;
     }
 };
 
@@ -100,7 +154,6 @@ const myMathObject = {
 /**
  * @param {...any} ??? - 任意の数の引数
  * @returns {Array<number>} 引数のうち、数値型のものだけを要素に持つ配列。要素の順番は引数で与えられた順番にする。
-
  */
 // test(myMathObject.getNumbers(1, 2, 3, 4), [1, 2, 3, 4]);
 // test(myMathObject.getNumbers("foo", 3, "4", "hi", 1), [3, 1]);
@@ -137,33 +190,117 @@ const MAX_EXPONENT = 5;
 
 // 以下のコードでは、一度にたくさんのテストしています。
 // （また、浮動小数点数の比較をするためにisNearlyEqualという関数を使っています。）
-for (let base = MIN_BASE; base <= MAX_BASE; ++base) {
-    for (let exponent = MIN_EXPONENT; exponent <= MAX_EXPONENT; ++exponent) {
-        const actual = myMathObject.power(base, exponent);
-        const expected = Math.pow(base, exponent);
-        if (isNearlyEqual(actual, expected)) {
-            console.log("Yay! Test PASSED.");
-        } else {
-            console.error("Test FAILED. Keep trying!");
-            console.log("    actual: ", actual);
-            console.log("  expected: ", expected);
-            console.trace();
-        }
-    }
-}
+// for (let base = MIN_BASE; base <= MAX_BASE; ++base) {
+//     for (let exponent = MIN_EXPONENT; exponent <= MAX_EXPONENT; ++exponent) {
+//         const actual = myMathObject.power(base, exponent);
+//         const expected = Math.pow(base, exponent);
+//         if (isNearlyEqual(actual, expected)) {
+//             console.log("Yay! Test PASSED.");
+//             // console.log("    actual: ", actual);
+//             // console.log("  expected: ", expected);
+//             // console.log("      base: ", base);
+//             // console.log("  exponent: ", exponent);
+//         } else {
+//             console.error("Test FAILED. Keep trying!");
+//             console.log("    actual: ", actual);
+//             console.log("  expected: ", expected);
+//             console.log("      base: ", base);
+//             console.log("  exponent: ", exponent);
+//             console.trace();
+//         }
+//     }
+// }
 
-// 参照文献： https://floating-point-gui.de/errors/comparison/
+// // 参照文献： https://floating-point-gui.de/errors/comparison/
 
-function isNearlyEqual(a, b) {
-    const absA = Math.abs(a);
-    const absB = Math.abs(b);
-    const diff = Math.abs(a - b);
+// function isNearlyEqual(a, b) {
+//     const absA = Math.abs(a);
+//     const absB = Math.abs(b);
+//     const diff = Math.abs(a - b);
 
-    if (a == b) {
-        return true;
-    } else if (a == 0 || b == 0 || absA + absB < Number.MIN_VALUE) {
-        return diff < Number.EPSILON * Number.MIN_VALUE;
-    } else {
-        return diff / Math.min(absA + absB, Number.MAX_VALUE) < Number.EPSILON;
-    }
-}
+//     if (a == b) {
+//         return true;
+//     } else if (a == 0 || b == 0 || absA + absB < Number.MIN_VALUE) {
+//         return diff < Number.EPSILON * Number.MIN_VALUE;
+//     } else {
+//         return diff / Math.min(absA + absB, Number.MAX_VALUE) < Number.EPSILON;
+//     }
+// }
+
+//##########################################################################################################
+//応用演習
+
+//1
+//myMathObject に ceil メソッドを追加してください。Math のビルトイン・メソッドを 使用してはいけません 。
+
+/**
+ * @param {number} ???
+ * @returns {number} 与えられた数字を切り上げた整数
+ */
+// test(myMathObject.ceil(1.1), 2);
+// test(myMathObject.ceil(2.2), 3);
+// test(myMathObject.ceil(3.3), 4);
+// test(myMathObject.ceil(4.4), 5);
+// test(myMathObject.ceil(5.5), 6);
+
+//2
+// myMathObject に floor メソッドを追加してください。Math のビルトインメソッドを 使用してはいけません 。
+
+/**
+ * @param {number} ???
+ * @returns {number} 与えられた数字を切り下げた整数
+ */
+// test(myMathObject.floor(1.1), 1);
+// test(myMathObject.floor(2.2), 2);
+// test(myMathObject.floor(3.3), 3);
+// test(myMathObject.floor(4.4), 4);
+// test(myMathObject.floor(5.5), 5);
+
+//3
+// myMathObject に round メソッドを追加してください。Math のビルトインメソッドを 使用してはいけません 。
+
+/**
+ * @param {number} ???
+ * @returns {number} 与えられた数字を四捨五入した整数
+ */
+// test(myMathObject.round(1.1), 1);
+// test(myMathObject.round(2.2), 2);
+// test(myMathObject.round(3.3), 3);
+// test(myMathObject.round(4.4), 4);
+// test(myMathObject.round(5.5), 6);
+// test(myMathObject.round(6.6), 7);
+// test(myMathObject.round(7.7), 8);
+
+//4
+// myMathObject に primeFactorization メソッドを追加してください。
+
+/**
+ * @param {number} ???
+ * @returns {{ [primeFactor: number]: number }} 引数の数値を素因数分解し、キーを _素因数_ 、値を対応する _指数_ にしたオブジェクト
+ */
+// test(myMathObject.primeFactorization(2), { 2: 1 });
+// test(myMathObject.primeFactorization(3), { 3: 1 });
+// test(myMathObject.primeFactorization(4), { 2: 2 });
+// test(myMathObject.primeFactorization(5), { 5: 1 });
+// test(myMathObject.primeFactorization(6), { 2: 1, 3: 1 });
+// test(myMathObject.primeFactorization(200560490130), {
+//     2: 1,
+//     3: 1,
+//     5: 1,
+//     7: 1,
+//     11: 1,
+//     13: 1,
+//     17: 1,
+//     19: 1,
+//     23: 1,
+//     29: 1,
+//     31: 1,
+// });
+// test(myMathObject.primeFactorization(900719925474099), {
+//     3: 1,
+//     53: 1,
+//     157: 1,
+//     1613: 1,
+//     2731: 1,
+//     8191: 1,
+// });
