be77de9c3084b2380553334b308b7d4afd13f4c8
yuta-shoji <90593209+yuta-shoji@users.noreply.github.com>
2022-03-30 17:51:36 +0900

    Add files via upload

---
+++ day-15/script-day-15.js
@@ -0,0 +1,38 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+//wormUp
+//ペアワークする中で良かった人推薦
+//seidaさん
+//ナビゲーターの時、丁寧に進めてくれる。進行しながら便利ツールを教えてくれる。
+//ドライバーの時、フォローしてくれる。
+
+//domの世界
+
+//機能が多い！！
+  // 1.関数の名前から何をしているのか汲み取る
+  // 2.実行して仮説を確かめる。
+  // 3.ドキュメントも調べて確かめる。
+
+
+//1
+// console.log(document.getElementsByTagName("p"));
+
+// console.log(document.getElementsByClassName("inner-paragraph"));
+
+// console.log(document.getElementById("main-header"));
+
+//演習
+//1
+
+function strUpper(str) {
+    return str.toUpperCase();
+}
+
+test(strUpper("hello123"), "HELLO123");
+
+document.querySelector("#main-header").innerHTML = "変更";
+
+function elementAllUpper(node) {
+    const elementArray = document.querySelector()
+}
